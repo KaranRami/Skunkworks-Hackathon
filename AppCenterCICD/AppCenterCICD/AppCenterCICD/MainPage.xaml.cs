a57814e2838dd296745fc86f8586f6d5402bec05
Karan Rami <krami@landmarkhealth.org>
2023-04-27 14:11:14 +0530

    APPCenter CI/CD projrct added

---
+++ AppCenterCICD/AppCenterCICD/AppCenterCICD/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCenterCICD
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogMessage_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CrashApp_Clicked(object sender, EventArgs e)
+        {
+            throw new Exception(entMessage.Text);
+        }
+    }
+}
